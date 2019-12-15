c6cae4d4531a47dcc0cae0d3025a10366c6720a5
vizeit <jigparekh@gmail.com>
2019-12-14 23:13:24 -0500

    testing multiprocesses

--- udp_packet_challenge/testmutiprocess.py
+++ udp_packet_challenge/testmutiprocess.py
@@ -1,24 +1,49 @@
 from multiprocessing import Process, Manager
+import time
+import sys
+
 
 def f(l, n):
-    l.append(n+1)
-    l.append(n+2)
+    try:
+        while True:
+            if len(l) < 100:
+                l[n] = n
+                n = n +1
+    except KeyboardInterrupt:
+        print "exiting f"
+        
+def f2():
+    try:
+        manager = Manager()
+        bloop = True
+        l = manager.dict()
+        count = 0
+        #print "Before creating process"
+        p = Process(target=f, args=(l, count))
+        p.start()
+        while bloop:
+            if len(l) > 0:
+                if bool(l) and count in l:
+                    print l[count]
+                    del l[count]
+                elif len(l) != 0:
+                    #print "key %d not found" % count
+                    #print l
+                    p.terminate()
+                    break
+                count = count + 1
+    except KeyboardInterrupt:
+        print "exiting f2"
+
 
 if __name__ == '__main__':
-    manager = Manager()
-
-    l = manager.list()
+    try:
+        p = Process(target=f2)
+        p.start()
+        while True:
+            pass
+    except KeyboardInterrupt:
+        print "interrupt in main"
+        sys.exit(0)
     
-
-    p = Process(target=f, args=(l, 0))
-    p.start()
-    p.join()
-    print l
-
-    p = Process(target=f, args=(l, 2))
-    p.start()
-    p.join()
-    print l
-
-
 
